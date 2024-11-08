e3e15dabb3f6c0098bad4b5cd726b90b8194043e
Garik <bbanan570@gmail.com>
2024-11-08 21:08:43 +0300

    WpfStart

--- MillionNumbersApp/MillionNumsConsoleApp/Program.cs
+++ MillionNumbersApp/MillionNumsConsoleApp/Program.cs
@@ -10,21 +10,22 @@ for (int i = 100000000; i <= 500000000; i += 100000000)
     stopWatch.Start();
     numberList.InitializeList(i);
     stopWatch.Stop();
-    Console.WriteLine("Список на " + i + " элементов создан за " + stopWatch.Elapsed.TotalSeconds + " секунд");
+    Console.WriteLine("\nСписок на " + i + " элементов создан за " + stopWatch.Elapsed.TotalSeconds + " секунд");
     stopWatch.Reset();
-}
 
-stopWatch.Start();
-Console.WriteLine("\nСумма: " + numberList.GetSumBasic());
-stopWatch.Stop();
-Console.WriteLine("Последовательное суммирование заняло " + stopWatch.Elapsed.TotalSeconds + " секунд\n\n");
-stopWatch.Reset();
-
-for (int i = 2; i <= maxProcCount; i++)
-{
     stopWatch.Start();
-    Console.WriteLine("\nСумма: " + numberList.GetSumPar(i));
+    Console.WriteLine("\nСумма: " + numberList.GetSumBasic());
     stopWatch.Stop();
-    Console.WriteLine("Используется " + i + " потоков\nПараллельное суммирование заняло " + stopWatch.Elapsed.TotalSeconds + " секунд");
+    Console.WriteLine("Последовательное суммирование заняло " + stopWatch.Elapsed.TotalSeconds + " секунд\n\n");
     stopWatch.Reset();
+
+    for (int p = 2; p <= maxProcCount; p++)
+    {
+        stopWatch.Start();
+        Console.WriteLine("\nСумма: " + numberList.GetSumPar(p));
+        stopWatch.Stop();
+        Console.WriteLine("Используется " + p + " потоков\nПараллельное суммирование заняло " + stopWatch.Elapsed.TotalSeconds + " секунд");
+        stopWatch.Reset();
+    }
 }
+
